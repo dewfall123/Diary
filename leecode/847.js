8eb55b6f060e076382fd6401ace8ceafd2a5f1f0
dewfall123 <1334767234@qq.com>
2019-02-14 08:53:41 +0800

    1

---
+++ leecode/847.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @param {*} graph 
+ */
+var shortestPathLength = function(graph) {
+    const N = graph.length;
+    // 动态规划
+    // 广搜
+    // 如果指定了起点结束点 直接广搜解决
+
+    // 初始化
+    const L = [];
+    for (const i = 0; i < N; i++) {
+        gI.foreach(j => {
+            l[i][j] = 1;
+        });
+    }
+    for () {
+        
+    }
+};
+
+const graph = [[1],[0,2,4],[1,3,4],[2],[1,2]];
+
+shortestPathLength(graph);
