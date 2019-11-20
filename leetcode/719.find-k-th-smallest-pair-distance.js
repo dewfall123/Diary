f795f7ed15a3fbc671f32c62bb23cbf64e87426a
zuofenghua <11100776@bbktel.com>
2019-11-20 21:32:05 +0800

    11.20

---
+++ leetcode/719.find-k-th-smallest-pair-distance.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=719 lang=javascript
+ *
+ * [719] Find K-th Smallest Pair Distance
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestDistancePair = function(nums, k) {
+    let heap = [];
+};
+
+smallestDistancePair([2,2,0,1,1,0,0,1,2,0], 2);
+// @lc code=end
+
