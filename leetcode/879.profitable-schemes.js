991f9e3edac575fc53a66379ba12ba64422c0184
zuofenghua <11100776@bbktel.com>
2019-12-12 21:43:13 +0800

    12.12

---
+++ leetcode/879.profitable-schemes.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=879 lang=javascript
+ *
+ * [879] Profitable Schemes
+ */
+
+// @lc code=start
+/**
+ * @param {number} G
+ * @param {number} P
+ * @param {number[]} group
+ * @param {number[]} profit
+ * @return {number}
+ */
+var profitableSchemes = function(G, P, group, profit) {
+    for(let i = 0; i < profit.length; i++) {
+        
+    }
+};
+// @lc code=end
+
