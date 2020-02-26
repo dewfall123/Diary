1efc7ec4e3c0ce454f96334eb49c530a07468fd2
dewfall123 <1334767234@qq.com>
2020-02-26 19:54:40 +0800

    02.26

---
+++ leetcode/1041.robot-bounded-in-circle.js
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode id=1041 lang=javascript
+ *
+ * [1041] Robot Bounded In Circle
+ *
+ * https://leetcode.com/problems/robot-bounded-in-circle/description/
+ *
+ * algorithms
+ * Medium (47.59%)
+ * Likes:    186
+ * Dislikes: 99
+ * Total Accepted:    11.8K
+ * Total Submissions: 24.8K
+ * Testcase Example:  '"GGLLGG"'
+ *
+ * On an infinite plane, a robot initially stands at (0, 0) and faces north.
+ * The robot can receive one of three instructions:
+ * 
+ * 
+ * "G": go straight 1 unit;
+ * "L": turn 90 degrees to the left;
+ * "R": turn 90 degress to the right.
+ * 
+ * 
+ * The robot performs the instructions given in order, and repeats them
+ * forever.
+ * 
+ * Return true if and only if there exists a circle in the plane such that the
+ * robot never leaves the circle.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "GGLLGG"
+ * Output: true
+ * Explanation: 
+ * The robot moves from (0,0) to (0,2), turns 180 degrees, and then returns to
+ * (0,0).
+ * When repeating these instructions, the robot remains in the circle of radius
+ * 2 centered at the origin.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "GG"
+ * Output: false
+ * Explanation: 
+ * The robot moves north indefinitely.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "GL"
+ * Output: true
+ * Explanation: 
+ * The robot moves from (0, 0) -> (0, 1) -> (-1, 1) -> (-1, 0) -> (0, 0) ->
+ * ...
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= instructions.length <= 100
+ * instructions[i] is in {'G', 'L', 'R'}
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
+var isRobotBounded = function(instructions) {
+    let position = { x: 0, y: 0, faces: 0 };
+    const instructions4 = instructions.repeat(4);
+    const handleGoStraight = {
+        0: p => p.y++,
+        1: p => p.x--,
+        2: p => p.y--,
+        3: p => p.x++,
+    }
+    const handleInstruction = {
+        G: p => handleGoStraight[p.faces](p),
+        L: p => p.faces = (p.faces + 1) % 4,
+        R: p => p.faces = (p.faces - 1 + 4) % 4,
+    };
+    for (let i of instructions4) {
+        handleInstruction[i](position);
+    }
+    return position.x === 0 && position.y === 0 && position.faces === 0;
+};
+// @lc code=end
+
+isRobotBounded('GLRLGLLGLGRGLGL');
