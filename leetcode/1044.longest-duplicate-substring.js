77f51211c95b63685cd7df45200eb40446578685
dewfall123 <1334767234@qq.com>
2020-03-04 21:07:11 +0800

    03.04

--- leetcode/1044.longest-duplicate-substring.js
+++ leetcode/1044.longest-duplicate-substring.js
@@ -8,6 +8,26 @@
  * @return {string}
  */
 var longestDupSubstring = function(S) {
-    
+    let result = '';
+    const map = {};
+    for (let i = 0; i < S.length; i++) {
+        const s = S[i];
+        if (map[s]) {
+            for (let preIndex of map[s]) {
+                let index = i;
+                let str = '';
+                while (index < S.length && S[preIndex] === S[index]) {
+                    str += S[index];
+                    index++;
+                    preIndex++;
+                }
+                if (result.length < str.length) {
+                    result = str;
+                }
+            }
+        } else {
+            map[s] = [i];
+        }
+    }
+    return result;
 };
-
