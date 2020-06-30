5ca9e18e542028d08edea652eea4d933ce634ca3
zuofenghua <11100776@bbktel.com>
2020-06-30 20:20:14 +0800

    06.30

--- leetcode/315.count-of-smaller-numbers-after-self.js
+++ leetcode/315.count-of-smaller-numbers-after-self.js
@@ -9,10 +9,40 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var countSmaller = function(nums) {
-    for () {
-        
+var countSmaller = function (nums) {
+  function Node(i) {
+    this.val = i;
+    this.dup = 1;
+    this.leftCount = 0;
+    this.left = null;
+    this.right = null;
+  }
+
+  const results = [];
+  let root = null;
+
+  function insertBst(val, node, sCount = 0) {
+    if (!node) {
+      node = new Node(val);
+      results.unshift(sCount);
+    } else if (val === node.val) {
+      node.dup++;
+      results.unshift(sCount + node.leftCount);
+    } else if (val > node.val) {
+      node.right = insertBst(val, node.right, sCount + node.dup + node.leftCount);
+    } else {
+      node.leftCount++;
+      node.left = insertBst(val, node.left, sCount);
     }
+    return node;
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const val = nums[i];
+    root = insertBst(val, root);
+  }
+
+  return results;
 };
 // @lc code=end
-
+countSmaller([5,2,6,1]);
